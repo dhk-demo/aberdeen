d33c10f1f37f02ed40cb5d4d75e3d8d9417f1803
Jeon <juhjeon@deloitte.com.hk>
2022-08-24 12:47:33 +0800

    okta second commit

--- aberdeen/settings.py
+++ aberdeen/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'users.apps.UserConfig',
     'social_django',
     'forum',
+    'django_saml2_auth',
 
 
 ]
@@ -122,6 +123,40 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+
+SAML2_AUTH = {
+    # Metadata is required, choose either remote url or local file path
+    'METADATA_AUTO_CONF_URL': "https://dhk-demo.oktapreview.com/app/dhk-demo_aberdeenapp_2/exk4os3psyDPVDlkB1d7/sso/saml" ,
+    
+    
+    # Optional settings below
+    'DEFAULT_NEXT_URL': '/worklist',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
+    'CREATE_USER': 'TRUE', # Create a new Django user when a new user logs in. Defaults to True.
+    'NEW_USER_PROFILE': {
+        'USER_GROUPS': [],  # The default group name when a new user logs in
+        'ACTIVE_STATUS': True,  # The default active status for new users
+        'STAFF_STATUS': True,  # The staff status for new users
+        'SUPERUSER_STATUS': False,  # The superuser status for new users
+    },
+    'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
+        'email': 'Email',
+        'username': 'UserName',
+        'first_name': 'FirstName',
+        'last_name': 'LastName',
+    },
+    'TRIGGER': {
+        'CREATE_USER': 'path.to.your.new.user.hook.method',
+        'BEFORE_LOGIN': 'path.to.your.login.hook.method',
+    },
+
+    
+    'ASSERTION_URL': 'http://localhost:8000', #'https://mysite.com', # Custom URL to validate incoming SAML requests against
+    'ENTITY_ID': 'http://localhost:8000/saml2_auth/acs/', # 'https://mysite.com/saml2_auth/acs/', # Populates the Issuer element in authn request
+    'NAME_ID_FORMAT': 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddres', # Sets the Format property of authn NameIDPolicy element
+    'USE_JWT': False, # Set this to True if you are running a Single Page Application (SPA) with Django Rest Framework (DRF), and are using JWT authentication to authorize client users
+    'FRONTEND_URL': 'http://localhost:8000', #'https://myfrontendclient.com', # Redirect URL for the client if you are using JWT auth with DRF. See explanation below
+    
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
