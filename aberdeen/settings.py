6d8df8444f22b2ecd385e09d9627af1c6ceaad25
Jeon <juhjeon@deloitte.com.hk>
2022-10-19 13:32:12 +0800

    commit

--- aberdeen/settings.py
+++ aberdeen/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -207,6 +207,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
